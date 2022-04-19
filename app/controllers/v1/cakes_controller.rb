d3186c98a02abf6676d1256262f0b9e0e910d533
Guilherme Fujii <gui.fujii@outlook.com>
2022-04-18 21:55:06 -0300

    feat: find cake by flavor

--- app/controllers/v1/cakes_controller.rb
+++ app/controllers/v1/cakes_controller.rb
@@ -1,9 +1,37 @@
 class V1::CakesController < ApplicationController
     def index
         if @cakes = Cake.all
-            render json: { message: 'Got all cakes', httpCode: 200, result: @cakes }, status: :ok
+            render json: { message: "Got all cakes", httpCode: 200, result: @cakes }, status: :ok
         else 
-            render json: { message: 'INTERNAL_SERVER_ERROR', httpCode: 500 }, status: 500
+            render json: { message: "INTERNAL_SERVER_ERROR", httpCode: 500 }, status: :internal_server_error
+        end
+    end
+
+    def create
+        @cake = Cake.new(cake_params)
+
+        if @cake.save
+            render json: { message: "New cake saved", httpCode: 201, result: @cake}, status: :created
+        end
+    end
+
+    def destroy
+        @cake = Cake.find(params[:id])
+
+        if @cake.present?
+            if @cake.destroy
+                head(:ok)
+            end
+        end
+    end
+
+    def find_by_flavor
+        @cakes = Cake.where(:flavor => params[:flavor])
+
+        if @cakes.present?
+            render json: { message: "Returned flavor filter", httpCode: 200, result: @cakes }, status: :ok
+        else 
+            render json: { message: "NOT_FOUND", httpCode: 404 }, status: :not_found
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace 'v1' do 
     resources :cakes
+    get "/cakes/flavor/:flavor", to: 'cakes#find_by_flavor'
   end
 end
